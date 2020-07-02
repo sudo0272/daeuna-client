483e19f0a98225213833fc242042ee7c9b8aebb4
sudo180272 <sudo180272@protonmail.ch>
2020-07-03 01:10:48 +0900

    Add constants of articulation methods

---
+++ tts/ArticulationMethod.py
@@ -0,0 +1,10 @@
+from enum import Enum
+
+class ArticulationMethod(Enum):
+    NASAL = 0
+    AFFRICATE_PLAIN = 1
+    AFFRICATE_TENSE = 2
+    AFFRICATE_ASPIRATED = 3
+    FRICATIVE_PLAIN = 4
+    FRICATIVE_TENSE = 5
+    LIQUID = 6
