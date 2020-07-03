c1addecfabb084ec9634aa7e9c67e264001b48fb
sudo180272 <sudo180272@protonmail.ch>
2020-07-03 17:07:42 +0900

    Make all member variables to private variables

--- tts/Consonant.py
+++ tts/Consonant.py
@@ -7,133 +7,134 @@ NO_SOUND = ''
 
 class Consonant:
     def __init__(self, letter: str, letter_type: LetterType):
-        self.letter = letter
+        self.__letter = letter
+        self.__letter_type = letter_type
 
         # Set plain, tense and aspirated
-        if letter in ('ㄱ', 'ㄲ', 'ㅋ'):
-            self.plain = 'ㄱ'
-            self.tense = 'ㄲ'
-            self.aspirated = 'ㅋ'
+        if self.__letter in ('ㄱ', 'ㄲ', 'ㅋ'):
+            self.__plain = 'ㄱ'
+            self.__tense = 'ㄲ'
+            self.__aspirated = 'ㅋ'
             
-        elif letter in ('ㄷ', 'ㄸ', 'ㅌ'):
-            self.plain = 'ㄷ'
-            self.tense = 'ㄸ'
-            self.aspirated = 'ㅌ'
+        elif self.__letter in ('ㄷ', 'ㄸ', 'ㅌ'):
+            self.__plain = 'ㄷ'
+            self.__tense = 'ㄸ'
+            self.__aspirated = 'ㅌ'
 
-        elif letter in ('ㅂ', 'ㅃ', 'ㅍ'):
-            self.plain = 'ㅂ'
-            self.tense = 'ㅃ'
-            self.aspirated = 'ㅍ'
+        elif self.__letter in ('ㅂ', 'ㅃ', 'ㅍ'):
+            self.__plain = 'ㅂ'
+            self.__tense = 'ㅃ'
+            self.__aspirated = 'ㅍ'
 
-        elif letter in ('ㅅ', 'ㅆ'):
-            self.plain = 'ㅅ'
-            self.tense = 'ㅆ'
-            self.aspirated = NO_SOUND
+        elif self.__letter in ('ㅅ', 'ㅆ'):
+            self.__plain = 'ㅅ'
+            self.__tense = 'ㅆ'
+            self.__aspirated = NO_SOUND
 
-        elif letter in ('ㅈ', 'ㅉ', 'ㅊ'):
-            self.plain = 'ㅈ'
-            self.tense = 'ㅉ'
-            self.aspirated = 'ㅊ'
+        elif self.__letter in ('ㅈ', 'ㅉ', 'ㅊ'):
+            self.__plain = 'ㅈ'
+            self.__tense = 'ㅉ'
+            self.__aspirated = 'ㅊ'
         
         else:
-            self.plain = NO_SOUND
-            self.tense = NO_SOUND
-            self.aspirated = NO_SOUND
+            self.__plain = NO_SOUND
+            self.__tense = NO_SOUND
+            self.__aspirated = NO_SOUND
         
         # Determine articulation method
-        if letter in ArticulationMethod.PLOSIVE_PLAIN_CONSONANTS:
-            self.articulation_method = ArticulationMethod.PLOSIVE_PLAIN
+        if self.__letter in ArticulationMethod.PLOSIVE_PLAIN_CONSONANTS:
+            self.__articulation_method = ArticulationMethod.PLOSIVE_PLAIN
 
-        elif letter in ArticulationMethod.PLOSIVE_TENSE_CONSONANTS:
-            self.articulation_method = ArticulationMethod.PLOSIVE_TENSE
+        elif self.__letter in ArticulationMethod.PLOSIVE_TENSE_CONSONANTS:
+            self.__articulation_method = ArticulationMethod.PLOSIVE_TENSE
 
-        elif letter in ArticulationMethod.PLOSIVE_ASPIRATED_CONSONANTS:
-            self.articulation_method = ArticulationMethod.PLOSIVE_ASPIRATED
+        elif self.__letter in ArticulationMethod.PLOSIVE_ASPIRATED_CONSONANTS:
+            self.__articulation_method = ArticulationMethod.PLOSIVE_ASPIRATED
 
-        elif letter in ArticulationMethod.AFFRICATE_PLAIN_CONSONANTS:
-            self.articulation_method = ArticulationMethod.AFFRICATE_PLAIN
+        elif self.__letter in ArticulationMethod.AFFRICATE_PLAIN_CONSONANTS:
+            self.__articulation_method = ArticulationMethod.AFFRICATE_PLAIN
 
-        elif letter in ArticulationMethod.AFFRICATE_TENSE_CONSONANTS:
-            self.articulation_method = ArticulationMethod.AFFRICATE_TENSE
+        elif self.__letter in ArticulationMethod.AFFRICATE_TENSE_CONSONANTS:
+            self.__articulation_method = ArticulationMethod.AFFRICATE_TENSE
 
-        elif letter in ArticulationMethod.AFFRICATE_ASPIRATED_CONSONANTS:
-            self.articulation_method = ArticulationMethod.AFFRICATE_ASPIRATED
+        elif self.__letter in ArticulationMethod.AFFRICATE_ASPIRATED_CONSONANTS:
+            self.__articulation_method = ArticulationMethod.AFFRICATE_ASPIRATED
 
-        elif letter in ArticulationMethod.FRICATIVE_PLAIN_CONSONANTS:
-            self.articulation_method = ArticulationMethod.FRICATIVE_PLAIN
+        elif self.__letter in ArticulationMethod.FRICATIVE_PLAIN_CONSONANTS:
+            self.__articulation_method = ArticulationMethod.FRICATIVE_PLAIN
 
-        elif letter in ArticulationMethod.FRICATIVE_TENSE_CONSONANTS:
-            self.articulation_method = ArticulationMethod.FRICATIVE_TENSE
+        elif self.__letter in ArticulationMethod.FRICATIVE_TENSE_CONSONANTS:
+            self.__articulation_method = ArticulationMethod.FRICATIVE_TENSE
 
-        elif letter in ArticulationMethod.NASAL_CONSONANTS:
-            self.articulation_method = ArticulationMethod.NASAL
+        elif self.__letter in ArticulationMethod.NASAL_CONSONANTS:
+            self.__articulation_method = ArticulationMethod.NASAL
 
-        elif letter in ArticulationMethod.LIQUID_CONSONANTS:
-            self.articulation_method = ArticulationMethod.LIQUID
+        elif self.__letter in ArticulationMethod.LIQUID_CONSONANTS:
+            self.__articulation_method = ArticulationMethod.LIQUID
 
         else:
-            self.articulation_method = ArticulationMethod.INVALID_LETTER
+            self.__articulation_method = ArticulationMethod.INVALID_LETTER
 
 
         # Determine aspiration position
-        if letter in ArticulationPostion.BILABIAL_CONSONANTS:
-            self.articulation_position = ArticulationPostion.BILABIAL
+        if self.__letter in ArticulationPostion.BILABIAL_CONSONANTS:
+            self.__articulation_position = ArticulationPostion.BILABIAL
         
-        elif letter in ArticulationPostion.ALVEOLAR_CONSONANTS:
-            self.articulation_position = ArticulationPostion.ALVEOLAR
+        elif self.__letter in ArticulationPostion.ALVEOLAR_CONSONANTS:
+            self.__articulation_position = ArticulationPostion.ALVEOLAR
         
-        elif letter in ArticulationPostion.PALATAL_CONSONANTS:
-            self.articulation_position = ArticulationPostion.PALATAL
+        elif self.__letter in ArticulationPostion.PALATAL_CONSONANTS:
+            self.__articulation_position = ArticulationPostion.PALATAL
         
-        elif letter in ArticulationPostion.VELAR_CONSONANTS:
-            self.articulation_position = ArticulationPostion.VELAR
+        elif self.__letter in ArticulationPostion.VELAR_CONSONANTS:
+            self.__articulation_position = ArticulationPostion.VELAR
         
-        elif letter in ArticulationPostion.GLOTTAL_CONSONANTS:
-            self.articulation_position = ArticulationPostion.GLOTTAL
+        elif self.__letter in ArticulationPostion.GLOTTAL_CONSONANTS:
+            self.__articulation_position = ArticulationPostion.GLOTTAL
         
         else:
-            self.articulation_position = ArticulationPostion.INVALID_LETTER  # All consonants except Bottom Consonants in korean are evinced
+            self.__articulation_position = ArticulationPostion.INVALID_LETTER  # All consonants except Bottom Consonants in korean are evinced
         
         # Determine the representative sound of the letter only if the letter is a bottom letter
-        if letter_type == LetterType.JONGSUNG:
-            if letter in BottomConsonantRepresentativeSound.GIYUK_CONSONANTS:
-                self.bottom_consonant_representative_sound = BottomConsonantRepresentativeSound.GIYUK
+        if self.__letter_type == LetterType.JONGSUNG:
+            if self.__letter in BottomConsonantRepresentativeSound.GIYUK_CONSONANTS:
+                self.__bottom_consonant_representative_sound = BottomConsonantRepresentativeSound.GIYUK
             
-            elif letter in BottomConsonantRepresentativeSound.NIEUN_CONSONANTS:
-                self.bottom_consonant_representative_sound = BottomConsonantRepresentativeSound.NIEUN
+            elif self.__letter in BottomConsonantRepresentativeSound.NIEUN_CONSONANTS:
+                self.__bottom_consonant_representative_sound = BottomConsonantRepresentativeSound.NIEUN
             
-            elif letter in BottomConsonantRepresentativeSound.DIGUT_CONSONANTS:
-                self.bottom_consonant_representative_sound = BottomConsonantRepresentativeSound.DIGUT
+            elif self.__letter in BottomConsonantRepresentativeSound.DIGUT_CONSONANTS:
+                self.__bottom_consonant_representative_sound = BottomConsonantRepresentativeSound.DIGUT
             
-            elif letter in BottomConsonantRepresentativeSound.RIEUL_CONSONANTS:
-                self.bottom_consonant_representative_sound = BottomConsonantRepresentativeSound.RIEUL
+            elif self.__letter in BottomConsonantRepresentativeSound.RIEUL_CONSONANTS:
+                self.__bottom_consonant_representative_sound = BottomConsonantRepresentativeSound.RIEUL
 
-            elif letter in BottomConsonantRepresentativeSound.MIEUM_CONSONANTS:
-                self.bottom_consonant_representative_sound = BottomConsonantRepresentativeSound.MIEUM
+            elif self.__letter in BottomConsonantRepresentativeSound.MIEUM_CONSONANTS:
+                self.__bottom_consonant_representative_sound = BottomConsonantRepresentativeSound.MIEUM
 
-            elif letter in BottomConsonantRepresentativeSound.IEUNG_CONSONANTS:
-                self.bottom_consonant_representative_sound = BottomConsonantRepresentativeSound.IEUNG
+            elif self.__letter in BottomConsonantRepresentativeSound.IEUNG_CONSONANTS:
+                self.__bottom_consonant_representative_sound = BottomConsonantRepresentativeSound.IEUNG
 
             else:
-                self.bottom_consonant_representative_sound = BottomConsonantRepresentativeSound.INVALID_LETTER
+                self.__bottom_consonant_representative_sound = BottomConsonantRepresentativeSound.INVALID_LETTER
 
         else:
-            self.bottom_consonant_representative_sound = BottomConsonantRepresentativeSound.NOT_BOTTOM_CONSONANT
+            self.__bottom_consonant_representative_sound = BottomConsonantRepresentativeSound.NOT_BOTTOM_CONSONANT
 
     def to_plain(self):
-        return self.plain
+        return self.__plain
     
     def to_tense(self):
-        return self.tense
+        return self.__tense
     
     def to_aspirated(self):
-            return self.aspirated
+            return self.__aspirated
     
     def get_articulation_method(self):
-        return self.articulation_method
+        return self.__articulation_method
     
     def get_articulation_position(self):
-        return self.articulation_position
+        return self.__articulation_position
 
     def get_bottom_consonant_representative_sound(self):
-        return self.bottom_consonant_representative_sound
+        return self.__bottom_consonant_representative_sound
