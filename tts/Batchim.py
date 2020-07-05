1378c09419b98e1b9af88a6b3236fe1b9e872a0f
sudo180272 <sudo180272@protonmail.ch>
2020-07-05 21:06:22 +0900

    Hardcode batchim

--- tts/Batchim.py
+++ tts/Batchim.py
@@ -1,47 +1,41 @@
 from tts.Consonant import Consonant
 
 class Batchim(Consonant):
-    GIYUK = 0
     GIYUK_CONSONANTS = ('ㄱ', 'ㄲ', 'ㅋ', 'ㄳ', 'ㄺ')
-    NIEUN = 1
     NIEUN_CONSONANTS = ('ㄴ', 'ㄵ')
-    DIGUT = 2
     DIGUT_CONSONANTS = ('ㄷ', 'ㅅ', 'ㅆ', 'ㅈ', 'ㅊ', 'ㅌ')
-    RIEUL = 3
     RIEUL_CONSONANTS = ('ㄹ', 'ㄼ', 'ㄽ', 'ㄾ')
-    MIEUM = 4
     MIEUM_CONSONANTS = ('ㅁ', 'ㄻ')
-    BIEUP = 5
     BIEUP_CONSONANTS = ('ㅂ', 'ㅍ', 'ㅄ', 'ㄿ')
-    IEUNG = 6
     IEUNG_CONSONANTS = ('ㅇ')
-    NOT_BOTTOM_CONSONANT = 7
-    INVALID_LETTER = 8
 
     def __init__(self, batchim: str):
         super.__init__()
         self.__batchim = batchim
 
         if self.__batchim in self.GIYUK_CONSONANTS:
-            self.__bottom_consonant_representative_sound = self.GIYUK
+            self.__bottom_consonant_representative_sound = 'ㄱ'
         
         elif self.__batchim in self.NIEUN_CONSONANTS:
-            self.__bottom_consonant_representative_sound = self.NIEUN
+            self.__bottom_consonant_representative_sound = 'ㄴ'
         
         elif self.__batchim in self.DIGUT_CONSONANTS:
-            self.__bottom_consonant_representative_sound = self.DIGUT
+            self.__bottom_consonant_representative_sound = 'ㄷ'
         
         elif self.__batchim in self.RIEUL_CONSONANTS:
-            self.__bottom_consonant_representative_sound = self.RIEUL
+            self.__bottom_consonant_representative_sound = 'ㄹ'
 
         elif self.__batchim in self.MIEUM_CONSONANTS:
-            self.__bottom_consonant_representative_sound = self.MIEUM
+            self.__bottom_consonant_representative_sound = 'ㅁ'
+
+        elif self.__batchim in self.BIEUP_CONSONANTS:
+            self.__bottom_consonant_representative_sound = 'ㅂ'
 
         elif self.__batchim in self.IEUNG_CONSONANTS:
-            self.__bottom_consonant_representative_sound = self.IEUNG
+            self.__bottom_consonant_representative_sound = 'ㅇ'
 
         else:
-            self.__bottom_consonant_representative_sound = self.INVALID_LETTER
+            self.__bottom_consonant_representative_sound = ''
 
     def get_representative_sound(self):
         return self.__bottom_consonant_representative_sound
